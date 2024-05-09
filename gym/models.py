2015c725a020542b33fe0e2d9e71959eb5d79562
PVxZeus <ghoneyaunglinn.trn@gmail.com>
2024-05-09 09:52:37 +0700

    first commit

---
+++ gym/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+# Create your models here.
+class Blog(models.Model):
+    title = models.CharField(max_length=255)
+    related_link = models.URLField(max_length=255)
+    content = models.TextField()
+    image = models.ImageField(upload_to="blog")
+    date_time = models.DateTimeField()
+
+    def __str__(self):
+        return self.title
